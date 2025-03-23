78b9a419e39b71cda26daad36e5fee79aeabc82c
Younghun Roh <yhunroh@gmail.com>
2025-03-22 20:42:34 -0700

    Fix gpt api

--- scentra-web/pages/api/openai.js
+++ scentra-web/pages/api/openai.js
@@ -2,15 +2,13 @@ import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
-const openai = new OpenAI();
-
 const ContentSchema = z.object({
   description: z.string(),
   instruction: z.string(),
 });
 
 const client = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY, // Ensure this environment variable is set
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // Ensure this environment variable is set
 });
 
 const prompt = `
